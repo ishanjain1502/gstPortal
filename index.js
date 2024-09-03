e5e6bd2e3c71a53a855d3b5ef9eb7d8e809e844f
ishanjain1502 <ishanjain1502@gmail.com>
2024-09-03 21:40:56 +0530

    fixes

--- index.js
+++ index.js
@@ -19,7 +19,20 @@ lowerLimit = parseInt(lowerLimit);
 let upperLimit = args[3] || 1000;
 upperLimit = parseInt(upperLimit);
 
-let district = args[4] || 'Hyderabad';
+let district = [
+    "ADILABAD",
+    "HYDERABAD",
+    "HYDERABAD (SOUTH)",
+    "KARIMNAGAR",
+    "KHAMMAM",
+    "MAHABOOBNAGAR",
+    "MEDAK AT PATANCHERU",
+    "NALGONDA",
+    "NIZAMABAD",
+    "RANGA REDDY",
+    "MEDCHAL-MALKAJGIRI",
+    "WARANGAL"
+]
 
 let DBhash = crypto.randomBytes(4).toString('hex');
 
@@ -64,181 +77,219 @@ db.run(`CREATE TABLE IF NOT EXISTS firms (
 
  (async () => {
     const browser = await puppeteer.launch({ headless: false }); // Launch browser in non-headless mode
-    const page = await browser.newPage(); // Open a new page
+    let page = await browser.newPage(); // Open a new page
     await page.goto('https://registration.telangana.gov.in/firmRegistration.htm', { 'waitUntil': 'networkidle2' }); // Replace with your website's URL
-
+    let newPage;
     // ---> this is where loop starts
 
-    for(let j=2024; j>=upperLimit ; j--){
-    // reg no
-    for(let i=1; i<=lowerLimit ; i++){
+    for(let k=0; k<district.length; k++){
 
-    // wait for page to complete loading
-    await page.waitForSelector('#accordion > div:nth-child(5) > div.panel-heading > h4 > a');
-    // wait for 2 more seconds
+        for(let j=2024; j>=upperLimit ; j--){
+        // reg no
+            for(let i=1; i<=lowerLimit ; i++){
 
-    // check if this selector is present
-    let isPresent = await page.evaluate(() => {
-        return document.querySelector('#accordion > div:nth-child(5) > div.panel-heading > h4 > a') !== null;
-    });
+            // wait for page to complete loading
+            await page.waitForSelector('#accordion > div:nth-child(5) > div.panel-heading > h4 > a');
+            // wait for 2 more seconds
 
-    if(!isPresent){
-        process.exit(0);
-    }
-    if(isPresent){
-        await page.evaluate(() => {
-            document.querySelector('#collapse6 > div > a').click();
-        });
-        await delay(5000);
-    }
+            // check if this selector is present
+            let isPresent = await page.evaluate(() => {
+                return document.querySelector('#accordion > div:nth-child(5) > div.panel-heading > h4 > a') !== null;
+            });
 
-
-    // when you click on the button, it opens a new tab
-    // you need to switch to that tab
-    // and then perform the actions
-    const [newPage] = await Promise.all([
-        new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page()))),
-        page.evaluate(() => {
-            document.querySelector('#collapse6 > div > a').click();
-        })
-    ]);
-
-    // wait until all the network requests are completed
-    await newPage.waitForNetworkIdle();
-    await delay(5000);
-
-    // Wait for the new tab to load
-    await newPage.waitForSelector('#firmRegistrationNo'); // Replace with your input field's selector
-
-    // Fill in the input box
-    await newPage.type('#firmRegistrationNo', `${i}`); // Replace with your input field's selector and text
-    await newPage.type('#regYear', `${j}`);
-    await newPage.select('#registrationDistrictName', district);
-    
-    
-    // Perform more actions on the new page if needed
-    await newPage.click('#Submit'); // Example of another action
-    
-    const result = await Promise.race([
-        newPage.waitForSelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > thead > tr > th:nth-child(1) > label').then(() => 'firstSelector'),
-        newPage.waitForSelector('#firmRegBean > div:nth-child(1) > div.form-grid > span').then(() => 'secondSelector')
-    ]);
-
-    if(result == 'firstSelector'){
-        console.log('firstSelector');
-    }
-    else{
-        console.log('secondSelector');
-    }
-
-    if(result == 'secondSelector'){
-        // continue;
-        console.log('not found');
-        process.exit(0);
-    }
-    
-    await newPage.waitForSelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > tbody > tr > td:nth-child(5) > a');
-
-    // check if this selector is present
-    isPresent = await newPage.evaluate(() => {
-        return document.querySelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > tbody > tr > td:nth-child(5) > a') !== null;
-    });
-    if(!isPresent){
-        console.log('not found');
-        process.exit(0);
-    }
-    if(isPresent){
-        console.log('found');
-        await newPage.evaluate(() => {
-            document.querySelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > tbody > tr > td:nth-child(5) > a').click();
-        });
-    }
-    
-
-    // wait for 5 seconds
-    await delay(5000);
-    await newPage.waitForSelector('body > section > div.container-fluid');
-    
-    await newPage.waitForSelector('#currentTableBodyId');
-    let data = [];
-    data = await newPage.evaluate(() => {
-        let data = [];
-        let firstNameEle = document.querySelector('#firstName');
-        let surNameEle = document.querySelector('#surName1');
-        let gender = document.querySelector('#gender');
-        let doorNo = document.querySelector('#doorNo');
-        let city = document.querySelector('#city');
-        let street = document.querySelector('#street');
-        let state = document.querySelector('#stateName');
-        let country = document.querySelector('#countryName');
-
-        // contact details
-        let phoneNumber = document.querySelector('#phoneNumber');
-        let emailId = document.querySelector('#emailId');
-        let mobileNumber = document.querySelector('#mobileNumber');
-
-        // firm details
-        let registrationNo = document.querySelector('#firmRegistrationNo');
-        let registeredDistrict = document.querySelector('#registrationDistrictName');
-        let registeredName = document.querySelector('#firmName');
-        let registeredYear = document.querySelector('#firmEndDate');
-
-
-        // firm partners:
-        let partnersTable = document.querySelector('#currentTableBodyId');
-        let rows = partnersTable.querySelectorAll('tr');
-        let partners = [];
-            for (let row of rows) {
-                let partner = {};
-                let name = row.querySelector('td:nth-child(2)').innerText;
-                let surName = row.querySelector('td:nth-child(3)').innerText;
-                let age = row.querySelector('td:nth-child(4)').innerText;
-                let address = row.querySelector('td:nth-child(5)').innerText;
-                partner = { name, surName, age, address };
-                partners.push(partner);
+            if(!isPresent){
+                process.exit(0);
+            }
+            if(isPresent){
+                await page.evaluate(() => {
+                    document.querySelector('#collapse6 > div > a').click();
+                });
+                await delay(5000);
             }
 
-            let object = {
-                firstName: firstNameEle.value,
-                surName: surNameEle.value,
-                gender: gender.value,
-                doorNo: doorNo.value,
-                city: city.value,
-                street: street.value,
-                state: state.value,
-                country: country.value,
-                phoneNumber: phoneNumber.value,
-                emailId: emailId.value,
-                mobileNumber: mobileNumber.value,
-                registrationNo: registrationNo.value,
-                registeredDistrict: registeredDistrict.value,
-                registeredName: registeredName.value,
-                registeredYear: registeredYear.value,
-                partners: partners
-            }
-            data.push(object);
-            return data;
-        })
-        
-        // inster data[0] into the table
-        db.run(`INSERT INTO firms (firstName, surName, gender, doorNo, city, street, state, country, phoneNumber, emailId, mobileNumber, registrationNo, 
-            registeredDistrict, registeredName, registeredYear, partners) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
-            [data[0].firstName, data[0].surName, data[0].gender, data[0].doorNo, data[0].city, 
-            data[0].street, data[0].state, data[0].country, data[0].phoneNumber, data[0].emailId, data[0].mobileNumber, data[0].registrationNo, 
-            data[0].registeredDistrict, data[0].registeredName, data[0].registeredYear, JSON.stringify(data[0].partners)], 
-            function(err) {
-                if (err) {
-                    console.error(err.message);
-                }
-                console.log('Data inserted.');
-            }
-        );
 
-    // Close the new tab
-    await newPage.close();
-    // ---> this is where loop ends
-    }
+            // when you click on the button, it opens a new tab
+            // you need to switch to that tab
+            // and then perform the actions
+            [newPage] = await Promise.all([
+                new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page()))),
+                page.evaluate(() => {
+                    document.querySelector('#collapse6 > div > a').click();
+                })
+            ]);
+
+            // wait until all the network requests are completed
+            await newPage.waitForNetworkIdle();
+            await delay(5000);
+
+            // Wait for the new tab to load
+            await newPage.waitForSelector('#firmRegistrationNo'); // Replace with your input field's selector
+
+            // Fill in the input box
+            await newPage.type('#firmRegistrationNo', `${i}`); // Replace with your input field's selector and text
+            await newPage.type('#regYear', `${j}`);
+            await newPage.select('#registrationDistrictName', district[k]);
+            
+            
+            // Perform more actions on the new page if needed
+            await newPage.click('#Submit'); // Example of another action
+            
+            const result = await Promise.race([
+                newPage.waitForSelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > thead > tr > th:nth-child(1) > label').then(() => 'firstSelector'),
+                newPage.waitForSelector('#firmRegBean > div:nth-child(1) > div.form-grid > span').then(() => 'secondSelector')
+            ]);
+
+            if(result == 'firstSelector'){
+                console.log('firstSelector');
+            }
+            else{
+                console.log('secondSelector');
+            }
+
+            if(result == 'secondSelector'){
+                // continue;
+                console.log('not found');
+                // process.exit(0);
+                break;
+            }
+            
+            await newPage.waitForSelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > tbody > tr > td:nth-child(5) > a');
+
+            // check if this selector is present
+            isPresent = await newPage.evaluate(() => {
+                return document.querySelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > tbody > tr > td:nth-child(5) > a') !== null;
+            });
+            if(!isPresent){
+                console.log('not found');
+                process.exit(0);
+            }
+            if(isPresent){
+                console.log('found');
+                await newPage.evaluate(() => {
+                    document.querySelector('#firmRegBean > div:nth-child(1) > div:nth-child(2) > div > table > tbody > tr > td:nth-child(5) > a').click();
+                });
+            }
+            
+
+            // wait for 5 seconds
+            await delay(5000);
+            try{
+                await newPage.waitForSelector('body > section > div.container-fluid');
+            
+            await newPage.waitForSelector('#currentTableBodyId');
+            }catch(err){
+                console.log('not found');
+                continue;
+            }
+            let data = [];
+            data = await newPage.evaluate(() => {
+                let data = [];
+                let firstNameEle = document.querySelector('#firstName');
+                let surNameEle = document.querySelector('#surName1');
+                let gender = document.querySelector('#gender');
+                let doorNo = document.querySelector('#doorNo');
+                let city = document.querySelector('#city');
+                let street = document.querySelector('#street');
+                let state = document.querySelector('#stateName');
+                let country = document.querySelector('#countryName');
+
+                // contact details
+                let phoneNumber = document.querySelector('#phoneNumber');
+                let emailId = document.querySelector('#emailId');
+                let mobileNumber = document.querySelector('#mobileNumber');
+
+                // firm details
+                let registrationNo = document.querySelector('#firmRegistrationNo');
+                let registeredDistrict = document.querySelector('#registrationDistrictName');
+                let registeredName = document.querySelector('#firmName');
+                let registeredYear = document.querySelector('#firmEndDate');
+
+
+                // firm partners:
+                let partnersTable = document.querySelector('#currentTableBodyId');
+                let rows = partnersTable.querySelectorAll('tr');
+                let partners = [];
+                    for (let row of rows) {
+                        let partner = {};
+                        let name = row.querySelector('td:nth-child(2)').innerText;
+                        let surName = row.querySelector('td:nth-child(3)').innerText;
+                        let age = row.querySelector('td:nth-child(4)').innerText;
+                        let address = row.querySelector('td:nth-child(5)').innerText;
+                        partner = { name, surName, age, address };
+                        partners.push(partner);
+                    }
+
+                    let object = {
+                        firstName: firstNameEle.value,
+                        surName: surNameEle.value,
+                        gender: gender.value,
+                        doorNo: doorNo.value,
+                        city: city.value,
+                        street: street.value,
+                        state: state.value,
+                        country: country.value,
+                        phoneNumber: phoneNumber.value,
+                        emailId: emailId.value,
+                        mobileNumber: mobileNumber.value,
+                        registrationNo: registrationNo.value,
+                        registeredDistrict: registeredDistrict.value,
+                        registeredName: registeredName.value,
+                        registeredYear: registeredYear.value,
+                        partners: partners
+                    }
+                    data.push(object);
+                    return data;
+                })
+                
+                // inster data[0] into the table
+                db.run(`INSERT INTO firms (firstName, surName, gender, doorNo, city, street, state, country, phoneNumber, emailId, mobileNumber, registrationNo, 
+                    registeredDistrict, registeredName, registeredYear, partners) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
+                    [data[0].firstName, data[0].surName, data[0].gender, data[0].doorNo, data[0].city, 
+                    data[0].street, data[0].state, data[0].country, data[0].phoneNumber, data[0].emailId, data[0].mobileNumber, data[0].registrationNo, 
+                    data[0].registeredDistrict, data[0].registeredName, data[0].registeredYear, JSON.stringify(data[0].partners)], 
+                    function(err) {
+                        if (err) {
+                            console.error(err.message);
+                        }
+                        console.log('Data inserted.');
+                    }
+                );
+
+
+            // Close the new tab
+            await newPage.close();
+
+            // ---> this is where loop ends
+            }
+        }
     }
     // Close the browser
     await browser.close();
+
+    let jsonData = [];
+    // read all the data from db
+    db.all(`SELECT * FROM firms`, (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        }
+        rows.forEach(row => {
+            console.log(row);
+            jsonData.push(row);
+        });
+        let json = {
+            data: jsonData
+        }
+        // create hash of 6 letters
+        let hash = crypto.randomBytes(6).toString('hex');
+        // Write the JSON data to a file
+        fs.writeFileSync(`./jsonData/${DBhash}Example.json`, JSON.stringify(json, null, 2));
+    
+        console.log('Data has been written to gst_data.json');
+    });
+
+
+    // close the db
+    db.close();
+
 })();
+
